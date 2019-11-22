2b86ea22528198fa65ac378cc9bdec4a30077ab4
jack <jfelag@uvm.edu>
2019-11-22 13:48:44 -0500

    mutate added and small tweak to probabilities

--- ant.py
+++ ant.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+TIME = 100
 
 class Ant:
     
@@ -21,12 +22,10 @@ class Ant:
         #genome 
         # sensing area
         self.A = A
-        # repeat step prob
-        self.pR = pR
-        # target step prob
-        self.pT = pT
-        # pheromone step prob
-        self.pP = pP
+        # repeat step prob, target step prob, pheromone step prob
+        self.pVec = np.array([pR, pT, pP])
+        
+        self.fix_parameters()
         
         
     def move(self, grid):
@@ -35,7 +34,6 @@ class Ant:
             or towards food/nest
         '''
         states = getNeighborStates(grid)
-        pass
     
     
     def mutate(self):
@@ -43,6 +41,32 @@ class Ant:
         
         '''
         
-        pass
+        if np.random.random() < 0.1:
+            self.A += np.random.choice([-1, +1])
+            if self.A < 1:
+                self.A = 1
+        else:
+            pIdxToChange = np.random.choice([0,1,2])
+            #change selected prob to [0.9, 1.1] times its original value
+            self.pVec[pIdxToChange] *= 1 + (np.random.random() * 0.2 - 0.1)
+            self.pVec /= np.sum(pVec)
+        
+        self.fix_parameters()
+        
     
+    def fix_parameters(self):
+        '''
+        renormalize probability vector to be a valid PMF, and
+            update the aliveTime based on sensing area
+        '''
+        
+        self.pVec /= np.sum(pVec)
+        
+        #smallest sensing area -> alive for half the sim time
+        # decrease life by 5 time steps for each bump in radius
+        # todo: change self.A term to quadratic weighting since the sensing area scales like A^2
+        self.aliveTime = TIME/2 - 5*self.A + 5
+        
+        
+        
         
