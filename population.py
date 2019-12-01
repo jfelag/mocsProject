afee87583c87b74bd97dbb34a2c8b99e474cf260
Kelly Gothard <kellygothard@gmail.com>
2019-12-01 10:06:10 -0500

    data collection

--- population.py
+++ population.py
@@ -5,6 +5,7 @@ import config as c
 from copy import deepcopy
 from ant import Ant
 from environment import Environment
+import pandas as pd
 
 import matplotlib.pyplot as plt
 
@@ -50,6 +51,20 @@ class Population:
         self.p = [None] * self.popSize
         for i in range(self.popSize):
             self.p[i] = self.ind()
+            
+            
+            
+    def collect_data(self):
+        for ant in self.p:
+            d = [ant.xPos, ant.yPos, ant.fitness, ant.A, 
+                  ant.foodFlag, ant.pVec[0], ant.pVec[1], 
+                 ant.pVec[2]]
+            temp = pd.DataFrame.from_dict(data = d)
+            try:
+                data.append(temp)
+            except:
+                data = temp
+        return data
 
 
 
@@ -64,9 +79,10 @@ class Population:
         
         for t in range(c.TIME_STEPS):
             antPositions = self.get_ant_positions()
+            data = self.collect_data()
             env.update(antPositions)
             self.move(env.grid)
-            
+        
             if c.VISUALS:
                 
                 plt.figure()
@@ -74,8 +90,9 @@ class Population:
                 plt.savefig('./figs/fig%03d.png'%t)    
                 plt.close()
                 
+        data.to_csv('testdata.csv')
+                
         
-
 
     def selection(self):
         """
@@ -130,7 +147,7 @@ class Population:
     
     
     def get_ant_positions(self):
-        
+                
         return [(ant.xPos, ant.yPos) for ant in self.p]
     
     
