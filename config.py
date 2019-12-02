0238ba83b81c5657658b97536983d0d51c977b32
jack <jfelag@uvm.edu>
2019-12-02 10:21:54 -0500

    new parameters

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 NUM_GENS = 100
-GRID_SIZE = 100
+GRID_SIZE = 21 #needs to be at least 2*A + 1
 FOOD_INITIAL = 10
-POP_SIZE = 30
-TIME_STEPS = 500 #needs to be at least 4*grid_size
+POP_SIZE = 10
+TIME_STEPS = 250 #needs to be at least 4*grid_size
 FOOD_VALUE = 9
 NEST_VALUE = 1
 
