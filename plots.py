d3c39f8c43aaa65e3478e9c2c4aeabd944b60965
Kelly Gothard <kellygothard@gmail.com>
2019-12-01 20:48:44 -0500

    one line of data collected, used to test if plots work

--- plots.py
+++ plots.py
@@ -34,52 +34,52 @@ def read_files(datadir):
 def SA_dist_T(data, T, out):
     # Distribution of # ants by sensing area at T = T
     plt.figure(figsize = (8,6))
-    dist_sensing_areaT = data[data['t'] == T]
+    dist_sensing_areaT = data[data['timestep'] == T]
     #dist_sensing_areaT = data[data['t'] == max(data['t'])]
-    plt.hist(dist_sensing_areaT[' A'], density = True)
+    plt.hist(dist_sensing_areaT['A'], density = True)
     plt.title('Distribution of Ant Survival by Sensing Area at T = '+str(T))
     plt.xlabel('Sensing Area')
     plt.ylabel('Frequency')
-    plt.xlim(0,max(data[' A']))
+    plt.xlim(0,max(data['A']))
     plt.savefig(out+'dist_sensing_area'+str(T)+'.png')
     plt.close()
 
 
 def SA_timeseries(data, out):
     # Timeseries of # of ants, each line is a sensing area group
-    n = len(data[' A'].unique())
+    n = len(data['A'].unique())
     colors = pl.cm.jet(np.linspace(0,1,n))
     i = 0
     plt.figure(figsize = (8,6))
-    for A in data[' A'].unique():
-        sensing_area_timeseries = data[data[' A'] == A].groupby('t').count().reset_index()
-        plt.plot(sensing_area_timeseries['t'], sensing_area_timeseries[' A'], color = colors[i])
+    for A in data['A'].unique():
+        sensing_area_timeseries = data[data['A'] == A].groupby('timestep').count().reset_index()
+        plt.plot(sensing_area_timeseries['timestep'], sensing_area_timeseries['A'], color = colors[i])
         i += 1
     
     plt.title('Timeseries for Ant Survival by Sensing Area')
     plt.xlabel('Sensing Area')
     plt.ylabel('Frequency')
-    plt.xlim(0,max(data['t']))
+    plt.xlim(0,max(data['timestep']))
     plt.savefig(out+'frequency_timeseries.png')
     plt.close()
 
 
 def fitness_SA_timeseries(data, out):
     # Timeseries of average fitness, each line is a sensing area group
-    n = len(data[' A'].unique())
+    n = len(data['A'].unique())
     colors = pl.cm.jet(np.linspace(0,1,n))
     i = 0
     plt.figure(figsize = (8,6))
-    for A in data[' A'].unique():
-        fitness_timeseries = data[data[' A'] == A].groupby('t').mean().reset_index()
-        plt.plot(fitness_timeseries['t'], fitness_timeseries[' fitness'], color = colors[i])
+    for A in data['A'].unique():
+        fitness_timeseries = data[data['A'] == A].groupby('timestep').mean().reset_index()
+        plt.plot(fitness_timeseries['timestep'], fitness_timeseries['fitness'], color = colors[i])
         i += 1
     
     plt.title('Timeseries for Average Fitness by Sensing Area')
     plt.xlabel('Timesteps')
     plt.ylabel('Average Fitness')
-    plt.xlim(0,max(data['t']))
-    plt.ylim(0,max(data[' fitness']))
+    plt.xlim(0,max(data['timestep']))
+    plt.ylim(0,max(data['fitness']))
     plt.savefig(out+'fitness_timeseries.png')
     plt.close()
 
@@ -87,13 +87,13 @@ def fitness_SA_timeseries(data, out):
 def fitness_SA_scatter(data, out):
     # Distribution of average fitness by sensing area group
     plt.figure(figsize = (8,6))
-    dist_fitnessT = data[data['t'] == max(data['t'])].groupby(' A').mean().reset_index()
-    plt.scatter(dist_fitnessT[' A'], dist_fitnessT[' fitness'])
+    dist_fitnessT = data[data['timestep'] == max(data['timestep'])].groupby('A').mean().reset_index()
+    plt.scatter(dist_fitnessT['A'], dist_fitnessT['fitness'])
     plt.title('Average Fitness by Sensing Area at T = T')
     plt.xlabel('Sensing Area')
     plt.ylabel('Average Fitness')
-    plt.xlim(0,max(data[' A']))
-    plt.ylim(0,max(data[' fitness']))
+    plt.xlim(0,max(data['A']))
+    plt.ylim(0,max(data['fitness']))
     plt.savefig(out+'dist_fitnessT.png')
     plt.close()
 
@@ -102,7 +102,7 @@ def violin_fitness(data, out):
     # Violin plot of each sensing areas fitness distribution
     fig = plt.figure()
     fig, axes = plt.subplots(figsize = (8,6))
-    sns.violinplot(' A',' fitness', data=data, ax = axes)
+    sns.violinplot('A','fitness', data=data, ax = axes)
     axes.set_title('Distribution of Fitness by Sensing Area')
     #axes.yaxis.grid(True)
     axes.set_xlabel('Sensing Area')
