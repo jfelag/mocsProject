d30df25aab079583e57a19e46430f8d6c9bcb462
Kelly Gothard <kellygothard@gmail.com>
2019-12-01 20:14:01 -0500

    cli and cleaning

--- plots.py
+++ plots.py
@@ -3,6 +3,31 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 import numpy as np
 import seaborn as sns
+import glob
+import argparse
+
+def make_args():
+    description = 'Generalized jobs submitter for PBS on VACC. Tailored to jobs that can be chunked based on datetime.' \
+                  ' Scripts to be run MUST have -o output argument. \n Output will be saved in log files with the first 3' \
+                  ' characters of args.flexargs and the start date for the job'
+    parser = argparse.ArgumentParser(description=description,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i',
+                        '--inputdir',
+                        help='input directory',
+                        required=True,
+                        type=str)
+    parser.add_argument('-o',
+                        '--outdir',
+                        help='output directory (will be passed to args.script with -o argument)',
+                        required=True,
+                        type=str)
+    parser.add_argument('-s',
+                        '--seed',
+                        help='desired seed files to grab',
+                        required=False,
+                        default=None,
+                        type=str)s
+    return parser.parse_args()
 
 # Data:
 #### ant.xPos - x position
@@ -15,93 +40,97 @@ import seaborn as sns
 #### ant.pVec[2] - p of moving towards pheromone
 #### t - timestep
 
-data = pd.read_csv('fakedata.csv', index_col=False)
-print(data.columns)
 
-# Distribution of # ants by sensing area at T = 0
-plt.figure(figsize = (8,6))
-dist_sensing_area0 = data[data['t'] == 0]
-plt.hist(dist_sensing_area0[' A'], density = True)
-plt.title('Distribution of Ant Survival by Sensing Area at T = 0')
-plt.xlabel('Sensing Area')
-plt.ylabel('Frequency')
-plt.xlim(0,max(data[' A']))
-plt.savefig('figs/dist_sensing_area0.png')
-plt.close()
-
-# Distribution of # ants by sensing area at T = T
-plt.figure(figsize = (8,6))
-dist_sensing_areaT = data[data['t'] == max(data['t'])]
-plt.hist(dist_sensing_areaT[' A'], density = True)
-plt.title('Distribution of Ant Survival by Sensing Area at T = T')
-plt.xlabel('Sensing Area')
-plt.ylabel('Frequency')
-plt.xlim(0,max(data[' A']))
-plt.savefig('figs/dist_sensing_areaT.png')
-plt.close()
+def SA_dist_T(T, out):
+    # Distribution of # ants by sensing area at T = T
+    plt.figure(figsize = (8,6))
+    dist_sensing_areaT = data[data['t'] == T]
+    #dist_sensing_areaT = data[data['t'] == max(data['t'])]
+    plt.hist(dist_sensing_areaT[' A'], density = True)
+    plt.title('Distribution of Ant Survival by Sensing Area at T = '+str(T))
+    plt.xlabel('Sensing Area')
+    plt.ylabel('Frequency')
+    plt.xlim(0,max(data[' A']))
+    plt.savefig(out+'dist_sensing_area'+str(T)+'.png')
+    plt.close()
 
 
-# Timeseries of # of ants, each line is a sensing area group
-n = len(data[' A'].unique())
-colors = pl.cm.jet(np.linspace(0,1,n))
-i = 0
-plt.figure(figsize = (8,6))
-for A in data[' A'].unique():
-    sensing_area_timeseries = data[data[' A'] == A].groupby('t').count().reset_index()
-    plt.plot(sensing_area_timeseries['t'], sensing_area_timeseries[' A'], color = colors[i])
-    i += 1
-
-plt.title('Timeseries for Ant Survival by Sensing Area')
-plt.xlabel('Sensing Area')
-plt.ylabel('Frequency')
-plt.xlim(0,max(data['t']))
-plt.savefig('figs/frequency_timeseries.png')
-plt.close()
-
-
-# Timeseries of average fitness, each line is a sensing area group
-n = len(data[' A'].unique())
-colors = pl.cm.jet(np.linspace(0,1,n))
-i = 0
-plt.figure(figsize = (8,6))
-for A in data[' A'].unique():
-    fitness_timeseries = data[data[' A'] == A].groupby('t').mean().reset_index()
-    plt.plot(fitness_timeseries['t'], fitness_timeseries[' fitness'], color = colors[i])
-    i += 1
-
-plt.title('Timeseries for Average Fitness by Sensing Area')
-plt.xlabel('Timesteps')
-plt.ylabel('Average Fitness')
-plt.xlim(0,max(data['t']))
-plt.ylim(0,max(data[' fitness']))
-plt.savefig('figs/fitness_timeseries.png')
-plt.close()
+def SA_timeseries(out):
+    # Timeseries of # of ants, each line is a sensing area group
+    n = len(data[' A'].unique())
+    colors = pl.cm.jet(np.linspace(0,1,n))
+    i = 0
+    plt.figure(figsize = (8,6))
+    for A in data[' A'].unique():
+        sensing_area_timeseries = data[data[' A'] == A].groupby('t').count().reset_index()
+        plt.plot(sensing_area_timeseries['t'], sensing_area_timeseries[' A'], color = colors[i])
+        i += 1
+    
+    plt.title('Timeseries for Ant Survival by Sensing Area')
+    plt.xlabel('Sensing Area')
+    plt.ylabel('Frequency')
+    plt.xlim(0,max(data['t']))
+    plt.savefig(out+'frequency_timeseries.png')
+    plt.close()
 
 
-# Distribution of average fitness by sensing area group
-plt.figure(figsize = (8,6))
-dist_fitnessT = data[data['t'] == max(data['t'])].groupby(' A').mean().reset_index()
-print(dist_fitnessT[' fitness'])
-plt.scatter(dist_fitnessT[' A'], dist_fitnessT[' fitness'])
-plt.title('Distribution of Average Fitness by Sensing Area at T = T')
-plt.xlabel('Sensing Area')
-plt.ylabel('Average Fitness')
-plt.xlim(0,max(data[' A']))
-plt.ylim(0,max(data[' fitness']))
-plt.savefig('figs/dist_fitnessT.png')
-plt.close()
+def fitness_SA_timeseries(out):
+    # Timeseries of average fitness, each line is a sensing area group
+    n = len(data[' A'].unique())
+    colors = pl.cm.jet(np.linspace(0,1,n))
+    i = 0
+    plt.figure(figsize = (8,6))
+    for A in data[' A'].unique():
+        fitness_timeseries = data[data[' A'] == A].groupby('t').mean().reset_index()
+        plt.plot(fitness_timeseries['t'], fitness_timeseries[' fitness'], color = colors[i])
+        i += 1
+    
+    plt.title('Timeseries for Average Fitness by Sensing Area')
+    plt.xlabel('Timesteps')
+    plt.ylabel('Average Fitness')
+    plt.xlim(0,max(data['t']))
+    plt.ylim(0,max(data[' fitness']))
+    plt.savefig(out+'fitness_timeseries.png')
+    plt.close()
 
 
-# Violin plot of each sensing areas fitness distribution
-fig = plt.figure()
-fig, axes = plt.subplots(figsize = (8,6))
-print(min(data[' fitness']))
-sns.violinplot(' A',' fitness', data=data, ax = axes)
-axes.set_title('Distribution of Fitness by Sensing Area')
-#axes.yaxis.grid(True)
-axes.set_xlabel('Sensing Area')
-axes.set_ylim(0)
-axes.set_ylabel('Fitness')
+def fitness_SA_scatter(out):
+    # Distribution of average fitness by sensing area group
+    plt.figure(figsize = (8,6))
+    dist_fitnessT = data[data['t'] == max(data['t'])].groupby(' A').mean().reset_index()
+    plt.scatter(dist_fitnessT[' A'], dist_fitnessT[' fitness'])
+    plt.title('Average Fitness by Sensing Area at T = T')
+    plt.xlabel('Sensing Area')
+    plt.ylabel('Average Fitness')
+    plt.xlim(0,max(data[' A']))
+    plt.ylim(0,max(data[' fitness']))
+    plt.savefig(out+'dist_fitnessT.png')
+    plt.close()
 
-plt.savefig('figs/violin_fitness.png')
-plt.close()
+
+def violin_fitness(out):
+    # Violin plot of each sensing areas fitness distribution
+    fig = plt.figure()
+    fig, axes = plt.subplots(figsize = (8,6))
+    print(min(data[' fitness']))
+    sns.violinplot(' A',' fitness', data=data, ax = axes)
+    axes.set_title('Distribution of Fitness by Sensing Area')
+    #axes.yaxis.grid(True)
+    axes.set_xlabel('Sensing Area')
+    axes.set_ylim(0)
+    axes.set_ylabel('Fitness')
+    
+    plt.savefig(out+'violin_fitness.png')
+    plt.close()
+
+def main():
+    
+    for file in glob.glob()
+        data = pd.read_csv('fakedata.csv', index_col=False)
+
+    SA_dist_T(0, out)
+    SA_dist_T(max(data['timestep']), out)
+    
+
+if __name__=="__main__":
+    main()
